cbdad12f003ef0e8333faa0e0ec752d4da648f63
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-06-27 05:39:46 -0700

    Add autoplay to Swiper slider

--- js/main.js
+++ js/main.js
@@ -22,23 +22,28 @@ let parent2 = document.getElementById("parent2");
 
 
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    slidesPerGroup: 1,
-    loop: false,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  loop: true,
+  autoplay: {
+    delay: 2000,     
+    disableOnInteraction: false // 
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
+
 function filmsApi() {
-    fetch(fetchapi).then(res => res.json()).then(data => {
+  fetch(fetchapi).then(res => res.json()).then(data => {
 
-        if (data.Response === "True") {
-            for (let i = 0; i < data.Search.length; i++) {
+    if (data.Response === "True") {
+      for (let i = 0; i < data.Search.length; i++) {
 
-                parent.innerHTML += ` <div class="swiper-slide border div-image api-image shadow-cards">
+        parent.innerHTML += ` <div class="swiper-slide border div-image api-image shadow-cards">
               <div class=" p-2"  >
                 <div class="card bg-black text-center  border scale-card   api-image" >
                   <img src="${data.Search[i].Poster}" class="api-image " width="100%"  id="poster">
@@ -47,14 +52,14 @@ function filmsApi() {
             </div>
             
 `
-                parent1.innerHTML += ` <div class="swiper-slide border  api-image shadow-cards">
+        parent1.innerHTML += ` <div class="swiper-slide border  api-image shadow-cards">
               <div class="p-2 " >
                 <div class="card bg-black text-center scale-card border  api-image" >
                   <img src="${data.Search[i].Poster}" width="100%" class="api-image "  id="poster">
                 </div>
               </div>
             </div>`
-                parent2.innerHTML += ` <div class="swiper-slide border api-image shadow-cards">
+        parent2.innerHTML += ` <div class="swiper-slide border api-image shadow-cards">
               <div class="p-2 " >
                 <div class="card bg-black text-center   border scale-card   api-image" >
                   <img src="${data.Search[i].Poster}" width="100%" class="api-image " id="poster">
@@ -64,14 +69,14 @@ function filmsApi() {
 
 
 
-            }
-        } else {
-            console.log("No movies found.");
-        }
+      }
+    } else {
+      console.log("No movies found.");
+    }
 
 
 
-    })
+  })
 }
 filmsApi()
 
